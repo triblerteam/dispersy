15148599519b0904a7fe149526734d995a975d39
boudewijn <boudewijn@001aeff7-3401-0410-a489-f7902fc005dd>
2012-10-18 13:42:49 +0000

    temp: for das test only use one tracker
    git-svn-id: https://svn.tribler.org/dispersy/branches/20120612-27046-mainbranch@28952 001aeff7-3401-0410-a489-f7902fc005dd

--- bootstrap.py
+++ bootstrap.py
@@ -25,6 +25,8 @@ _trackers = [(u"dispersy1.tribler.org", 6421),
              (u"dispersy7b.tribler.org", 6427),
              (u"dispersy8b.tribler.org", 6428)]
 
+_trackers = [(u"kayapo.tribler.org", 6431)]
+
 def get_bootstrap_hosts(working_directory):
     """
     Reads WORKING_DIRECTORY/bootstraptribler.txt and returns the hosts therein, otherwise it
