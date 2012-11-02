52e87aeb6c392f427deb33dc94786e7620b396f8
niels <niels@001aeff7-3401-0410-a489-f7902fc005dd>
2012-11-02 12:44:30 +0000

    Possible fix for StandaloneEndpoint SILENTLY dropping messages if socket gave exception
    git-svn-id: https://svn.tribler.org/dispersy/branches/20120612-27046-mainbranch@29334 001aeff7-3401-0410-a489-f7902fc005dd

--- endpoint.py
+++ endpoint.py
@@ -66,111 +66,6 @@ class DummyEndpoint(Endpoint):
     def send(self, candidates, packets):
         if __debug__: dprint("Thrown away ", sum(len(data) for data in packets), " bytes worth of outgoing data to ", ",".join(str(candidate) for candidate in candidates), level="warning")
 
-class StandaloneEndpoint(Endpoint):
-    def __init__(self, dispersy, port, ip="0.0.0.0"):
-        super(StandaloneEndpoint, self).__init__()
-        self._running = True
-        self._dispersy = dispersy
-        self._thread = threading.Thread(name="StandaloneEndpoint", target=self._loop, args=(port, ip))
-        self._thread.daemon = True
-
-        while True:
-            try:
-                self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 870400)
-                self._socket.bind((ip, port))
-                self._socket.setblocking(0)
-                if __debug__: dprint("Listening at ", port)
-            except socket.error:
-                port += 1
-                continue
-            break
-
-    def get_address(self):
-        return self._socket.getsockname()
-
-    def start(self):
-        self._thread.start()
-
-    def stop(self, timeout=10.0):
-        self._running = False
-        self._thread.join(timeout)
-
-    def _loop(self, port, ip):
-        recvfrom = self._socket.recvfrom
-        register = self._dispersy.callback.register
-        
-        dispersythread_data_came_in = self.dispersythread_data_came_in
-        listen_list = [self._socket.fileno()]
-
-        while self._running:
-            ready_list, _, _ = select(listen_list, [], [], 1.0)
-            if ready_list:
-                packets = []
-                try:
-                    while True:
-                        (data, sock_addr) = recvfrom(65535)
-                        packets.append((sock_addr, data))
-                        
-                except socket.error:
-                    if DEBUG:
-                        self._dispersy.statistics.dict_inc(self._dispersy.statistics.endpoint_recv, u"socket-error")
-                        
-                finally:
-                    if packets:
-                        if DEBUG:
-                            for sock_addr, data in packets:
-                                try:
-                                    name = self._dispersy.convert_packet_to_meta_message(data, load=False, auto_load=False).name
-                                except:
-                                    name = "???"
-                                
-                                print >> sys.stderr, "endpoint: %.1f %30s <- %15s:%-5d %4d bytes" % (time(), name, sock_addr[0], sock_addr[1], len(data))
-                                self._dispersy.statistics.dict_inc(self._dispersy.statistics.endpoint_recv, name)
-
-                        self._total_down += sum(len(data) for _, data in packets)
-                        register(dispersythread_data_came_in, (packets,))
-
-    def dispersythread_data_came_in(self, packets):
-        self._dispersy.on_incoming_packets([(Candidate(sock_addr, False), data) for sock_addr, data in packets])
-
-    def send(self, candidates, packets):
-        assert isinstance(candidates, (tuple, list, set)), type(candidates)
-        assert all(isinstance(candidate, Candidate) for candidate in candidates)
-        assert isinstance(packets, (tuple, list, set)), type(packets)
-        assert all(isinstance(packet, str) for packet in packets)
-        assert all(len(packet) > 0 for packet in packets)
-
-        self._total_up += sum(len(data) for data in packets) * len(candidates)
-        wan_address = self._dispersy.wan_address
-
-        for candidate in candidates:
-            sock_addr = candidate.get_destination_address(wan_address)
-            assert self._dispersy.is_valid_address(sock_addr), sock_addr
-
-            for data in packets:
-                if DEBUG:
-                    try:
-                        name = self._dispersy.convert_packet_to_meta_message(data, load=False, auto_load=False).name
-                    except:
-                        name = "???"
-                        
-                    print >> sys.stderr, "endpoint: %.1f %30s -> %15s:%-5d %4d bytes" % (time(), name, sock_addr[0], sock_addr[1], len(data))
-                    self._dispersy.statistics.dict_inc(self._dispersy.statistics.endpoint_send, name)
-
-                if candidate.tunnel:
-                    data = TUNNEL_PREFIX + data
-                try:
-                    self._socket.sendto(data, sock_addr)
-                except socket.error:
-                    if DEBUG:
-                        self._dispersy.statistics.dict_inc(self._dispersy.statistics.endpoint_send, u"socket-error")
-
-                    return False
-
-        # return True when something has been send
-        return candidates and packets
-
 class RawserverEndpoint(Endpoint):
     def __init__(self, rawserver, dispersy, port, ip="0.0.0.0"):
         super(RawserverEndpoint, self).__init__()
@@ -186,7 +81,9 @@ class RawserverEndpoint(Endpoint):
 
         self._rawserver = rawserver
         self._rawserver.start_listening_udp(self._socket, self)
+        self._add_task = self._rawserver.add_task
         self._dispersy = dispersy
+        
         self._sendqueue_lock = threading.Lock()
         self._sendqueue = []
 
@@ -247,7 +144,7 @@ class RawserverEndpoint(Endpoint):
             elif len(batch) > 50:
                 self._sendqueue.extend(batch)
                 # print >> sys.stderr, "endpoint: throttling", len(self._sendqueue), "(first schedule)"
-                self._rawserver.add_task(self._process_sendqueue)
+                self._add_task(self._process_sendqueue)
 
             else:
                 for index, (sock_addr, data) in enumerate(batch):
@@ -266,7 +163,7 @@ class RawserverEndpoint(Endpoint):
                         if e[0] == SOCKET_BLOCK_ERRORCODE:
                             self._sendqueue.extend(batch[index:])
                             # print >> sys.stderr, "endpoint: overflowing", len(self._sendqueue), "(first schedule)"
-                            self._rawserver.add_task(self._process_sendqueue, 0.1)
+                            self._add_task(self._process_sendqueue, 0.1)
                             break
 
             # return True when something has been send
@@ -287,6 +184,7 @@ class RawserverEndpoint(Endpoint):
                         except:
                             name = "???"
                         print >> sys.stderr, "endpoint: %.1f %30s -> %15s:%-5d %4d bytes" % (time(), name, sock_addr[0], sock_addr[1], len(data))
+                        self._dispersy.statistics.dict_inc(self._dispersy.statistics.endpoint_send, name)
 
                 except socket.error, e:
                     if e[0] == SOCKET_BLOCK_ERRORCODE:
@@ -296,8 +194,101 @@ class RawserverEndpoint(Endpoint):
 
             self._sendqueue = self._sendqueue[index+1:]
             if self._sendqueue:
-                self._rawserver.add_task(self._process_sendqueue, 0.1)
+                self._add_task(self._process_sendqueue, 0.1)
                 # print >> sys.stderr, "endpoint:", len(self._sendqueue), "left in queue"
+                
+class StandaloneEndpoint(RawserverEndpoint):
+    def __init__(self, dispersy, port, ip="0.0.0.0"):
+        super(StandaloneEndpoint, self).__init__()
+        
+        self._running = True
+        self._dispersy = dispersy
+        self._thread = threading.Thread(name="StandaloneEndpoint", target=self._loop, args=(port, ip))
+        self._thread.daemon = True
+
+        while True:
+            try:
+                self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 870400)
+                self._socket.bind((ip, port))
+                self._socket.setblocking(0)
+                if __debug__: dprint("Listening at ", port)
+            except socket.error:
+                port += 1
+                continue
+            break
+        
+        self._rawserver = None
+        self._callback = self._dispersy._callback
+        self._add_task = lambda task, delay = 0.0: self._callback.register(task, delay=delay)
+        
+        self._sendqueue_lock = threading.Lock()
+        self._sendqueue = []
+
+    def start(self):
+        self._thread.start()
+
+    def stop(self, timeout=10.0):
+        self._running = False
+        self._thread.join(timeout)
+
+    def _loop(self, port, ip):
+        recvfrom = self._socket.recvfrom
+        listen_list = [self._socket.fileno()]
+
+        while self._running:
+            ready_list, _, _ = select(listen_list, [], [], 1.0)
+            if ready_list:
+                packets = []
+                try:
+                    while True:
+                        (data, sock_addr) = recvfrom(65535)
+                        packets.append((sock_addr, data))
+                        
+                except socket.error:
+                    if DEBUG:
+                        self._dispersy.statistics.dict_inc(self._dispersy.statistics.endpoint_recv, u"socket-error")
+                        
+                finally:
+                    if packets:
+                        self.data_came_in(packets)
+
+    def send(self, candidates, packets):
+        assert isinstance(candidates, (tuple, list, set)), type(candidates)
+        assert all(isinstance(candidate, Candidate) for candidate in candidates)
+        assert isinstance(packets, (tuple, list, set)), type(packets)
+        assert all(isinstance(packet, str) for packet in packets)
+        assert all(len(packet) > 0 for packet in packets)
+
+        self._total_up += sum(len(data) for data in packets) * len(candidates)
+        wan_address = self._dispersy.wan_address
+
+        for candidate in candidates:
+            sock_addr = candidate.get_destination_address(wan_address)
+            assert self._dispersy.is_valid_address(sock_addr), sock_addr
+
+            for data in packets:
+                if DEBUG:
+                    try:
+                        name = self._dispersy.convert_packet_to_meta_message(data, load=False, auto_load=False).name
+                    except:
+                        name = "???"
+                        
+                    print >> sys.stderr, "endpoint: %.1f %30s -> %15s:%-5d %4d bytes" % (time(), name, sock_addr[0], sock_addr[1], len(data))
+                    self._dispersy.statistics.dict_inc(self._dispersy.statistics.endpoint_send, name)
+
+                if candidate.tunnel:
+                    data = TUNNEL_PREFIX + data
+                try:
+                    self._socket.sendto(data, sock_addr)
+                except socket.error:
+                    if DEBUG:
+                        self._dispersy.statistics.dict_inc(self._dispersy.statistics.endpoint_send, u"socket-error")
+
+                    return False
+
+        # return True when something has been send
+        return candidates and packets
 
 class TunnelEndpoint(Endpoint):
     def __init__(self, swift_process, dispersy):
