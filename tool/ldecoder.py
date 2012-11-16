83814e3d495d4552ce3b2279366db916c3149d66
niels <niels@001aeff7-3401-0410-a489-f7902fc005dd>
2012-11-16 10:44:23 +0000

    git-svn-id: https://svn.tribler.org/dispersy/branches/20120612-27046-mainbranch@29657 001aeff7-3401-0410-a489-f7902fc005dd

--- tool/ldecoder.py
+++ tool/ldecoder.py
@@ -282,7 +282,7 @@ def parselast(filename, interests = (), raise_exceptions = True, chars = 2048):
     
     lines = f.readlines()
     lines.reverse()
-    print >> sys.stderr, lines[0]
+    print >> sys.stderr, lines
     return _parse(lines, set(interests), raise_exceptions)
 
 class NextFile(Exception):
