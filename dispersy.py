319f48a3464fd4cfe1733acc0e5b2094876e8195
niels <niels@001aeff7-3401-0410-a489-f7902fc005dd>
2012-11-19 10:45:59 +0000

    git-svn-id: https://svn.tribler.org/dispersy/branches/20120612-27046-mainbranch@29710 001aeff7-3401-0410-a489-f7902fc005dd

--- dispersy.py
+++ dispersy.py
@@ -2925,7 +2925,7 @@ ORDER BY meta_message.priority DESC, sync.global_time * meta_message.direction""
         
         if not result:
             import sys
-            candidates = meta.community.dispersy_yield_random_candidates()
+            candidates = list(meta.community.dispersy_yield_random_candidates())
             print >> sys.stderr, long(time()), "_forward failed", meta.name, type(meta.destination), len(candidates), messages
         return result
     
